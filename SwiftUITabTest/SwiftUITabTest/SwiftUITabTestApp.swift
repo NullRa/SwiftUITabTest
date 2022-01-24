c930c83466faf25ba1f93c4c5277eb512515ab0a
jyang <jTang@walkgame.com>
2022-01-24 12:23:16 +0800

    init project

---
+++ SwiftUITabTest/SwiftUITabTest/SwiftUITabTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITabTestApp.swift
+//  SwiftUITabTest
+//
+//  Created by Apple on 2022/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITabTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
